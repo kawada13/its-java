50454ef0555baccf99d0c3830413244b185a5e91
kawada <vgnsl94@gmail.com>
2022-06-06 22:56:01 +0900

    feature create creationForm

--- src/main/java/com/example/its/web/issue/issueController.java
+++ src/main/java/com/example/its/web/issue/issueController.java
@@ -5,17 +5,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/issues")
 public class issueController {
 
   private final IssueService issueService;
 
-  @GetMapping("/issues")
+  @GetMapping
   public String showList(Model model) {
 
     model.addAttribute("issueList", issueService.findAll());
     return "issues/list";
+  }
+
+  @GetMapping("/creationForm")
+  public String showCreationForm() {
+    return "issues/creationForm";
   }
 }
